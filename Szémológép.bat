813b048f22595ee884ffa477e8444c391ef7bf7b
Csaba <56678429+CsabaVK@users.noreply.github.com>
2020-01-20 20:16:41 +0100

    Add files via upload

---
+++ Szémológép.bat
@@ -0,0 +1,20 @@
+﻿@echo off
+@title szamologep
+setlocal EnableDelayedExpansion
+color 0f
+goto calc
+
+:calc
+cls
+echo osszeadas       = +
+echo kivonas       = -
+echo osztas         = /
+echo szorzas       = *
+echo -----------
+echo Feladat 
+set /p equ=
+set /a equ=!equ!
+cls
+echo megoldas:!equ!
+pause
+goto calc
